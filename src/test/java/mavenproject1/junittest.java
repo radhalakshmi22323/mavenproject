56fe639357edd3da928f9663f2eae39788018736
Balakrishna Setty <balakrishna.setty@zensar.com>
2022-04-22 14:10:29 +0530

    maven project test file

---
+++ src/test/java/mavenproject1/junittest.java
@@ -0,0 +1,14 @@
+package mavenproject1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class junittest {
+
+	@Test
+	public void test() {
+		System.out.println("test");
+	}
+
+}
